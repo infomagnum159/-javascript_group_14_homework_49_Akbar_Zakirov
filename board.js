2c0ede1f28a4737b08dec5608700ffea5913b768
Infom <dsfsd>
2022-05-19 17:45:43 +0600

    Added a second chessboard block

--- board.js
+++ board.js
@@ -1,7 +1,12 @@
-for(let i = 0; i < 4; i++) {
+for(let i = 0; i < 4; i++){
     let first = '';
-    for (let i = 0; i < 4; i++) {
+    let second = '';
+    for(let i = 0; i < 4; i++){
         first += '██  ';
     }
     console.log(first);
+    for(let i = 0; i < 4; i++){
+        second += '  ██';
+    }
+    console.log(second);
 }
